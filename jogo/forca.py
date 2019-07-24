d1aae72290503eeeb1a998aa841c7df98ad1be04
lucripa <l.cripa@gmail.com>
2019-07-23 22:56:03 -0300

    aula-2.2

---
+++ jogo/forca.py
@@ -0,0 +1,67 @@
+print('*****************')
+print('* Jogo da Froca *')
+print('*****************')
+
+print('\nVocê tem 7 tentativas para sair da forca!\n')
+
+palavra_secreta = 'banana'
+letra_acertadas = ['_','_','_','_','_','_',]
+
+enforcou = False
+acertou = False
+erros = 0
+tentativas = 1
+
+while(not enforcou and not acertou):
+
+    chute = input('Qual a letra: ')
+
+    if(chute.upper() in palavra_secreta.upper()):
+        posicao = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letra_acertadas[posicao] = letra.upper()
+            
+        
+            posicao += 1
+
+    else:
+        erros += 1
+        print('\nTentativa {}'.format(tentativas))
+        tentativas += 1
+
+    print(letra_acertadas, end='\n\n')
+
+    acertou = '_' not in letra_acertadas
+    enforcou = erros == 7
+
+    if(acertou ):
+        print('PARABENS! Você ganhou!')
+        break
+    elif(enforcou):
+        print('ENFORCADO')
+        break
+print('Fim do jogo.')
+
+#for posicao in range(0, len(texto))
+
+
+print('''
+|─|─────────────────|
+| |               (o.o)
+| |               _||_
+| |              / || \\
+| |             /  ||  \\
+| |                /\\
+| |              _/  \_
+| |
+|_|================================
+========== VOCÊ PERDEU !!! ========
+===================================
+''')
+
+a = 'python'
+
+a.split()
+
+print(a)
