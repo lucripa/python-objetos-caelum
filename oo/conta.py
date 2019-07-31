15074b7fbd89937822ebf43ae510dd7c3754958d
lucripa <l.cripa@gmail.com>
2019-07-30 21:22:55 -0300

    aula6

--- oo/conta.py
+++ oo/conta.py
@@ -1,20 +1,26 @@
 from cliente import *
 
 class Conta:
+
+    _total_conta = 0
+
+    __slots__ = ('_saldo', '_titular', '_limite', '_numero')
+
     def __init__(self, numero, titular, saldo, limite=1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
-    
+        self._numero = numero
+        self._titular = titular
+        self._saldo = saldo
+        self._limite = limite
+        Conta._total_conta += 1
+
     def deposita (self, valor):
-        self.saldo += valor
+        self._saldo += valor
     
     def saca(self, valor):
-        if (self.saldo < valor):
+        if (self._saldo < valor):
             return False
         else:
-            self.saldo -= valor
+            self._saldo -= valor
             return True
 
     def transfere_para(self, destino, valor):
@@ -26,4 +32,37 @@ class Conta:
             return True
 
     def extrato(self):
-        print('Numero: {}\nTitular: {}\nSaldo: {}\nLimite: {}'.format(self.numero, self.titular, self.saldo, self.limite))
+        print('Numero: {}\nTitular: {}\nSaldo: {}\nLimite: {}'.format(self._numero, self._titular, self._saldo, self._limite))
+    
+
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def titular(self):
+        return self._titular
+
+    @titular.setter
+    def titular(self, titular):
+        self._titular = titular
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @numero.setter
+    def numero(self, numero):
+        self._numero = numero
+
+    @property
+    def limite(self):
+        return self._limite
+
+    @limite.setter
+    def limite(self, limite):
+        self._limite = limite
+
+    @classmethod
+    def get_total_contas(cls):
+        return cls._total_conta
